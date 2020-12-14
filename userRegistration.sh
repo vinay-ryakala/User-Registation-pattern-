2870d663e7b3b808380bec6b08e50571b4d994c2
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-14 12:31:33 +0530

    Adding email pattern to validate UC3

--- userRegistration.sh
+++ userRegistration.sh
@@ -16,8 +16,11 @@ fi
 }
 read -p "give first name" firstName
 read -p "give last name" lastName
+read -p "emter email" email
 
 namePattern="^[A-Z]{1}[A-Za-z]{2,}$"
+emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})*$"
 
 pattern $firstName $namePattern
 pattern $lastName $namePattern
+pattern $email $emailPattern
