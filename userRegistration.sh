5300705f89d32fcfabd733686b8b7048ce4e498f
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-14 14:35:39 +0530

    adding Password pattern to check UC5

--- userRegistration.sh
+++ userRegistration.sh
@@ -18,12 +18,15 @@ read -p "give first name" firstName
 read -p "give last name" lastName
 read -p "enter email" email
 read -p "give mobile number" phone
+read -p  "give pass word" Password
 
 namePattern="^[A-Z]{1}[A-Za-z]{2,}$"
 emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})*$"
 phonePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+passwordPattern="^([@!#$%&*,.-_]*[a-zA-Z0-9]*){8,}$"
 
 pattern $firstName $namePattern
 pattern $lastName $namePattern
 pattern $email $emailPattern
 pattern "$phone" $phonePattern
+pattern $Password $passwordPattern
