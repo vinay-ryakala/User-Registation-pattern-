23525df884ce51886906cdb00c2bc954280b074a
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-14 12:19:58 +0530

    Adding last name  to check pattern UC2

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,13 +2,22 @@
 
 #welcome to user registration
 
+function pattern()
+{
+input=$1
+patternCheck=$2
+
+if [[ $input =~ $patternCheck ]]
+then
+   echo " $input  is valid input"
+else
+   echo "$input is Invaild  Input "
+fi
+}
 read -p "give first name" firstName
+read -p "give last name" lastName
 
 namePattern="^[A-Z]{1}[A-Za-z]{2,}$"
 
-if [[ $firstName =~ $namePattern ]]
-then
-   echo " $firstName  is valid input"
-else
-   echo "$firstName is Invaild  Input "
-fi
+pattern $firstName $namePattern
+pattern $lastName $namePattern
