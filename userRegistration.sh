6f10ad475ffddcdf7996bc19b367e96948c51b02
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-14 12:12:02 +0530

    Adding first name to validate pattern UC1

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,3 +2,13 @@
 
 #welcome to user registration
 
+read -p "give first name" firstName
+
+namePattern="^[A-Z]{1}[A-Za-z]{2,}$"
+
+if [[ $firstName =~ $namePattern ]]
+then
+   echo " $firstName  is valid input"
+else
+   echo "$firstName is Invaild  Input "
+fi
